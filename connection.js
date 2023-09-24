2981443028c87c3df24ec8e77a3b2f141d74576a
SandeeepS <sandeepsanthosh3303@gmail.com>
2023-09-24 10:47:14 +0530

    nine commit

--- connection.js
+++ connection.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 async function connectMongoDb(url){
-    return mongoose.connect(url);
+    return mongoose.connect(process.env.DB_CONNECT,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        useCreateIndex:true
+
+    }).then(()=>console.log("connected to mongodb"))
+    .catch((err)=>console.log("MongoDB connection failed"));
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+const env = require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const session = require('express-session');
 const routes = require('./routes/userRoute');
 const {connectMongoDb} = require('./connection');
+
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true }));
@@ -10,7 +14,7 @@ app.set('view engine','ejs');
 app.use(express.static("views"));
 app.use(express.static(__dirname));
 
-connectMongoDb("mongodb://127.0.0.1:27017/BoysWorld");
+
 //session
 const oneDay = 1000 * 60 * 60 * 24;
 //configuring express session
@@ -22,11 +26,23 @@ app.use(session({
   }));
   
 
+//mongoDb connection
+ mongoose.connect(process.env.DB_CONNECT,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+   
+
+}).then(()=>console.log("connected to mongodb"))
+.catch((err)=>console.log("MongoDB connection failed",err));
+
 app.use('/',routes);
 
-
-
-
-app.listen(5000,()=>{
-    console.log('server is running at :',5000);
+const port  = process.env.PORT || 5001
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
 });
+
+
+// app.listen(5000,()=>{
+//     console.log('server is running at :',5000);
+// });
