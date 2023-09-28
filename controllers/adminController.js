e4b7699c81355b800e135411232d483809393da8
SandeeepS <sandeepsanthosh3303@gmail.com>
2023-09-28 14:58:43 +0530

    thrd

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,3 +4,26 @@ const userModel = require('../models/userModel');
 exports.adminPage = async(req,res)=>{
     res.render('adminpanel/login');
 }
+
+exports.getDashboard = async(req,res)=>{
+    res.render('adminpanel/index');
+}
+
+exports.getCustomer = async(req,res)=>{
+    res.render('adminpanel/customers');
+}
+
+exports.getProduct = async(req,res)=>{
+    res.render('adminpanel/product');
+}
+
+exports.getCategories = async(req,res)=>{
+    res.render('adminpanel/categories');
+}
+exports.getOrders = async(req,res)=>{
+    res.render('adminpanel/orders');
+}
+
+exports.getBanner = async(req,res)=>{
+    res.render('adminpanel/banner');
+}
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -5,4 +5,11 @@ const adminController = require('../controllers/adminController');
 
 //adminroute
 adminRoute.get('/',adminController.adminPage);
+adminRoute.post('/dashboard',adminController.getDashboard);
+adminRoute.get('/dashboard',adminController.getDashboard);
+adminRoute.get('/customers',adminController.getCustomer);
+adminRoute.get('/products',adminController.getProduct);
+adminRoute.get('/categories',adminController.getCategories);
+adminRoute.get('/orders',adminController.getOrders);
+adminRoute.get('/banner',adminController.getBanner);
 module.exports = adminRoute;
